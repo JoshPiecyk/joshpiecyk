9042048247f96ffed8f15df5c86f840c11283aa4
Josh <jpiecyk@bu.edu>
2023-07-11 19:22:50 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,117 +1,233 @@
-var square = document.getElementById("square");
-var stepSize = 5; // Adjust this value to control the movement speed
-var acceleration = 0.1; // Adjust this value to control the acceleration rate
-var friction = 0.98; // Adjust this value to control the friction rate
-var intervalId = null;
-var momentumIntervalId = null;
-var squareTop = window.innerHeight / 2 - 25; // Start square in the center of the screen
-var squareLeft = window.innerWidth / 2 - 25;
-var velocityX = 0;
-var velocityY = 0;
-var objects = [];
-var objectCount = 0;
+window.onload = function() { //Hello Mr. Computer! If you are curious enough to look at the code
+  const canvas = document.getElementById("canvas"); //You deserve to know that S and W change the gravity strength
+  const ctx = canvas.getContext("2d");
+  let circles = [];
+  let isMouseDown = false;
+  let reverseGravity = false;
+  let gravity = 0.5;
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  let isRandomizing = false; // Flag to indicate randomize mode
 
-function createObject() {
-  var object = document.createElement("div");
-  object.className = "object";
-  object.style.top = Math.floor(Math.random() * window.innerHeight) + "px";
-  object.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
-  document.body.appendChild(object);
-  objects.push(object);
-}
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-function moveSquare() {
-  function updateSquarePosition() {
-    square.style.top = squareTop + "px";
-    square.style.left = squareLeft + "px";
-  }
+  canvas.addEventListener("mousedown", event => {
+    isMouseDown = true;
+  });
 
-  function checkCollision() {
-    var squareRect = square.getBoundingClientRect();
-    var squareCenterX = squareRect.left + squareRect.width / 2;
-    var squareCenterY = squareRect.top + squareRect.height / 2;
+  canvas.addEventListener("mouseup", event => {
+    isMouseDown = false;
+  });
 
-    for (var i = 0; i < objects.length; i++) {
-      var object = objects[i];
-      var objectRect = object.getBoundingClientRect();
-      var objectCenterX = objectRect.left + objectRect.width / 2;
-      var objectCenterY = objectRect.top + objectRect.height / 2;
-      var distanceX = squareCenterX - objectCenterX;
-      var distanceY = squareCenterY - objectCenterY;
-      var combinedHalfWidths = squareRect.width / 2 + objectRect.width / 2;
-      var combinedHalfHeights = squareRect.height / 2 + objectRect.height / 2;
+  canvas.addEventListener("mousemove", event => {
+    if (isMouseDown) {
+      const circle = {
+        x: event.clientX,
+        y: event.clientY,
+        radius: isRandomizing ? Math.floor(Math.random() * 16) + 5 : parseFloat(circleSizeSlider.value),
+        dy: 0,
+        gravity: reverseGravity ? -gravity : gravity,
+        elasticity: 0.9,
+        color: getRandomColor(),
+        hasCollided: false // Flag to track collision sound
+      };
 
-      if (
-        Math.abs(distanceX) < combinedHalfWidths &&
-        Math.abs(distanceY) < combinedHalfHeights
-      ) {
-        var angle = Math.atan2(distanceY, distanceX);
-        var force = stepSize * 0.5; // Adjust the force to control the object's movement
-        var forceX = Math.cos(angle) * force;
-        var forceY = Math.sin(angle) * force;
-        object.style.transform = `translate(${forceX}px, ${forceY}px) rotate(${Math.random() * 360}deg)`;
-      } else if (
-        Math.abs(distanceX) < combinedHalfWidths * 3 &&
-        Math.abs(distanceY) < combinedHalfHeights * 3
-      ) {
-        object.style.filter = "brightness(90%)";
-      } else {
-        object.style.filter = "";
+      circles.push(circle);
+    }
+  });
+
+  document.addEventListener("keydown", event => {
+    if (event.code === "Space") {
+      reverseGravity = !reverseGravity;
+
+      // Reverse gravity for existing circles
+      for (let i = 0; i < circles.length; i++) {
+        circles[i].gravity = reverseGravity ? -gravity : gravity;
       }
     }
-  }
 
-  document.addEventListener("keydown", function(event) {
-    if (intervalId !== null) return; // Prevent multiple intervals
-    intervalId = setInterval(function() {
-      switch (event.key) {
-        case "w":
-          velocityY -= acceleration;
-          break;
-        case "s":
-          velocityY += acceleration;
-          break;
-        case "a":
-          velocityX -= acceleration;
-          break;
-        case "d":
-          velocityX += acceleration;
-          break;
+    if (event.key === "w") {
+      gravity += 0.1;
+
+      // Update gravity for existing circles
+      for (let i = 0; i < circles.length; i++) {
+        circles[i].gravity = reverseGravity ? -gravity : gravity;
       }
-      updateSquarePosition();
-      checkCollision();
-    }, 10); // Adjust the interval time (in milliseconds) for smoother movement
+    }
+
+    if (event.key === "s") {
+      gravity -= 0.1;
+      gravity = Math.max(0, gravity);
+
+      // Update gravity for existing circles
+      for (let i = 0; i < circles.length; i++) {
+        circles[i].gravity = reverseGravity ? -gravity : gravity;
+      }
+    }
   });
 
-  document.addEventListener("keyup", function(event) {
-    clearInterval(intervalId);
-    intervalId = null;
-    if (momentumIntervalId !== null) return; // Prevent multiple momentum intervals
-    momentumIntervalId = setInterval(function() {
-      if (Math.abs(velocityX) < 0.1 && Math.abs(velocityY) < 0.1) {
-        clearInterval(momentumIntervalId);
-        momentumIntervalId = null;
-        velocityX = 0;
-        velocityY = 0;
+  function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < circles.length; i++) {
+      const circle = circles[i];
+
+      // Apply gravity
+      circle.dy += circle.gravity;
+
+      // Update circle position
+      circle.y += circle.dy;
+
+      // Check if the circle hits the bottom or top of the screen
+      if (circle.y + circle.radius > canvas.height) {
+        // Reverse the direction and reduce velocity due to bounce
+        circle.dy *= -circle.elasticity;
+
+        // Move the circle just above the bottom of the screen
+        circle.y = canvas.height - circle.radius;
+
+        // Play "plink" sound effect if not already collided
+        if (!circle.hasCollided) {
+          playSoundEffect(circle.radius, Math.abs(circle.dy));
+          circle.hasCollided = true;
+        }
+      } else if (circle.y - circle.radius < 0) {
+        // Reverse the direction and reduce velocity due to bounce
+        circle.dy *= -circle.elasticity;
+
+        // Move the circle just below the top of the screen
+        circle.y = circle.radius;
+
+        // Play "plink" sound effect if not already collided
+        if (!circle.hasCollided) {
+          playSoundEffect(circle.radius, Math.abs(circle.dy));
+          circle.hasCollided = true;
+        }
       } else {
-        squareTop += velocityY;
-        squareLeft += velocityX;
-        velocityX *= friction;
-        velocityY *= friction;
-        updateSquarePosition();
-        checkCollision();
+        // Reset collision flag if not resting on the ground or ceiling
+        circle.hasCollided = false;
       }
-    }, 10);
-  });
-}
 
-function spawnObject() {
-  if (objectCount < 1640) {
-    createObject();
-    objectCount++;
-    setTimeout(spawnObject, 6.9); // determines spawning speed, lower is faster, 1000 is 1 second)
+      // Draw circle with random shade of red, yellow, or orange and 25% opacity
+      ctx.beginPath();
+      ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2, false);
+      ctx.fillStyle = circle.color;
+      ctx.globalAlpha = 0.35;
+      ctx.fill();
+      ctx.closePath();
+
+      // Remove circles that are off-screen
+      if (circle.y - circle.radius > canvas.height) {
+        circles.splice(i, 1);
+        i--;
+      }
+    }
+
+    requestAnimationFrame(update);
   }
-}
 
-spawnObject();
-moveSquare();
+  update();
+
+  // Generate random color in the red, yellow, or orange spectrum
+  function getRandomColor() {
+    const colors = ["#FF3300", "#FF6600", "#FF9900", "#FFCC00", "#FFFF00"];
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+
+  // Play "plink" sound effect with variable pitch and volume based on circle size and impact force
+  function playSoundEffect(circleSize, impactForce) {
+    const oscillator = audioContext.createOscillator();
+    const gainNode = audioContext.createGain();
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+
+    // Calculate frequency based on circle size
+    const frequency = 1100 - circleSize * 40;
+
+    // Calculate volume based on impact force
+    const volume = Math.min(0.1, impactForce * 0.002);
+
+    oscillator.type = "sine";
+    oscillator.frequency.value = frequency;
+
+    gainNode.gain.value = volume;
+
+    oscillator.start();
+
+    // Stop the sound after 0.1 seconds
+    setTimeout(() => {
+      oscillator.stop();
+    }, 100);
+  }
+
+  const resetButton = document.getElementById("resetButton");
+  resetButton.addEventListener("click", () => {
+    circles = [];
+  });
+
+  const circleSizeSlider = document.getElementById("circleSizeSlider");
+  circleSizeSlider.addEventListener("input", () => {
+    // Update circle size for new circles
+  });
+
+  const randomizeToggle = document.getElementById("randomizeToggle");
+  randomizeToggle.addEventListener("change", () => {
+    isRandomizing = randomizeToggle.checked;
+    toggleSwitchLabel.style.backgroundColor = isRandomizing ? "#6dd36d" : "#ccc";
+  });
+
+  const toggleSwitchLabel = document.querySelector(".toggle-switch-label");
+  toggleSwitchLabel.style.fontStyle = "italic";
+  toggleSwitchLabel.style.color = "#fcba03";
+
+  const gravityText = document.createElement("div");
+  gravityText.textContent = "Press Space!";
+  gravityText.style.position = "fixed";
+  gravityText.style.top = "50%";
+  gravityText.style.left = "50%";
+  gravityText.style.transform = "translate(-50%, -50%)";
+  gravityText.style.fontFamily = "Courier New, Courier, monospace";
+  gravityText.style.fontSize = "69px";
+  gravityText.style.fontWeight = "bold";
+  gravityText.style.color = "#fcba03";
+  gravityText.style.opacity = "0";
+  document.body.appendChild(gravityText);
+
+  setTimeout(() => {
+    fadeIn(gravityText, 9000);
+  }, 11000);
+
+  function fadeIn(element, duration) {
+    let opacity = 0;
+    const interval = 10;
+    const delta = interval / duration;
+
+    const timer = setInterval(() => {
+      opacity += delta;
+      element.style.opacity = opacity;
+
+      if (opacity >= 1) {
+        clearInterval(timer);
+        setTimeout(() => {
+          fadeOut(element, 1000);
+        }, 1000);
+      }
+    }, interval);
+  }
+
+  function fadeOut(element, duration) {
+    let opacity = 1;
+    const interval = 10;
+    const delta = interval / duration;
+
+    const timer = setInterval(() => {
+      opacity -= delta;
+      element.style.opacity = opacity;
+
+      if (opacity <= 0) {
+        clearInterval(timer);
+      }
+    }, interval);
+  }
+};
